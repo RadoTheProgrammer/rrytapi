e279c2e9639a43f5b9b00ab0ea3df5b4c510a249
RadoTheProgrammer <rado@arazakar.com>
2024-02-12 12:55:00 +0100

    tested simple_compare

--- examples/05_convert_audio.py
+++ examples/05_convert_audio.py
@@ -2,4 +2,6 @@
 To convert audio
 """
 import rrytapi
-rrytapi.convert_audio()
+rrytapi.convert_audio(
+    "/Users/alain/RR/Rpy/rrytapi/rrytapi_downloads/JVKE_-_this_is_what_autumn_feels_like_251.weba"
+)
--- simple_compare.py
+++ simple_compare.py
@@ -1,11 +1,40 @@
-import rrytapi,requests
-ti=rrytapi.TimeIt()
-v=rrytapi.Video.get("https://www.youtube.com/watch?v=M-mtdN6R3bQ")
+
+import rrytapi
+import requests
+from rrytapi.utils import TimeIt
+
+ti=TimeIt()
+
+v=rrytapi.get_video("https://www.youtube.com/watch?v=M-mtdN6R3bQ")
 url=v.formats.url
 
 ti.print("GET...")
-a=requests.get(url)
-ti.print("CONTENT...")
-b=a.content
+with requests.get(url,stream=True) as res:
+    ti.print("CONTENT...")
+    res.raise_for_status()
+    for chunk in res.iter_content(chunk_size=8192):
+        pass
+
 ti.print("DONE")
+
+"""
+stream=False
+001.012 001.012 GET...
+030.007 028.995 CONTENT...
+030.007 000.000 DONE
+
+stream=True
+000.829 000.829 GET...
+000.865 000.036 CONTENT...
+029.783 028.918 DONE
+
+chunk_size=8192
+028.916
+028.977
+028.918
+
+chunk_size=1024
+028.937
+028.917
+"""
 
